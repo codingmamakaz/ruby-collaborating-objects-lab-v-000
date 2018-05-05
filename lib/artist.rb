c102856f063441cca0c59b298d4f321e01dd2a14
Kazumi Karbowski <kazkazinus@gmail.com>
2018-05-05 03:32:43 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -23,13 +23,7 @@ class Artist
   end
 
   def self.find_or_create_by_name(name)
-    self.all.detect do |artist|
-      if artist.name == name
-        name
-    else
-      Artist.new(name)
-      end
-    end
+    self.all.detect {|artist| artist.name == name} || Artist.new(name)
   end
 
   def print_songs
