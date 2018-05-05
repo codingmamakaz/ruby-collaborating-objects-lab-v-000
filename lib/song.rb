f8c807ab5a128d890891b8c95fbdd42a2b9ef48b
Kazumi Karbowski <kazkazinus@gmail.com>
2018-05-05 02:59:55 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -7,19 +7,10 @@ class Song
     @name = name
   end
 
-  def artist_name=(name)
-
-  Artist.find_or_create_by_name(name)
-  Artist.add_song(song)
-  end
-
-
-
-
   def self.new_by_filename(filename)
     #This will return "Black or White"
-
     title = filename.split(" - ")[1]
+    song = self.new(title)
     name = filename.split(" - ")[0]
     artist_name=(name)
     song = self.new(title)
