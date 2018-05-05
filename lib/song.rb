066a957362c3baa61dc3060132d9f89338bb1efd
Kazumi Karbowski <kazkazinus@gmail.com>
2018-05-05 03:34:33 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,7 +9,6 @@ class Song
 
   def self.new_by_filename(filename)
     song = self.new(filename.split(" - ")[1])
-    # artist = Artist.find_or_create_by_name(filename.split(" - ")[0])
     artist = Artist.find_or_create_by_name(filename.split(" - ")[0])
     song.artist = artist
     artist.add_song(song)
