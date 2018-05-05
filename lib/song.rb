c52ac0364a01cd6de8de40a34216aa015baf4756
Kazumi Karbowski <kazkazinus@gmail.com>
2018-05-05 03:17:44 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -12,6 +12,7 @@ binding.pry
     song = self.new(filename.split(" - ")[1])
     # artist = Artist.find_or_create_by_name(filename.split(" - ")[0])
     artist_name = filename.split(" - ")[0]
+    artist = Artist.find_or_create_by_name(artist_name)
     song.artist = artist
     artist.add_song(song)
     song
